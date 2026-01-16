54aa91274d6076a637b6c0fb859ede1dff2699c0
kachore <89995425+kachore@users.noreply.github.com>
2026-01-16 18:36:42 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,10 +8,10 @@ SECRET_KEY = 'replace-this-with-a-secure-secret-in-production'
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '72.62.187.189',
+
     'eejong.org',
     'www.eejong.org',
-    '.ngrok-free.dev',
+    
 ]
 
 
@@ -113,5 +113,6 @@ CSRF_COOKIE_HTTPONLY = True  # Empêche accès JS (bonne pratique)
 # FEDAPAY_MODE = os.environ.get('FEDAPAY_MODE', 'sandbox')
 
 # FEDAPAY_WEBHOOK_SECRET = os.environ.get('FEDAPAY_WEBHOOK_SECRET')
+
 
 
